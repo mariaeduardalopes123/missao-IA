757da13a0bedfe979b28f57be2eaf7c0109906a6
mariaeduardalopes123 <108529862+mariaeduardalopes123@users.noreply.github.com>
2024-09-27 12:14:31 -0300

    oi

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const textoResultado = document.querySelector(".texto-resultado");
 // Array de objetos contendo as perguntas e alternativas
 const perguntas = [
      {
-     enunciado: "Qual é o oceano que divide o Brasil?"
+     enunciado: "Qual é o oceano que divide o Brasil?",
      alternativas: [
           "oceano Atlântico",
           "oceano Pacífico"
@@ -15,7 +15,7 @@ const perguntas = [
      correta: 0 // A primeira alternativa é a correta
 },
 {
-     enunciado: "A maquiagem pode se usar no corpo além do rosto?"
+     enunciado: "A maquiagem pode se usar no corpo além do rosto?",
      alternativas: [
           "não",
           "sim"
@@ -23,7 +23,7 @@ const perguntas = [
      correta: 1 // A segunda alternativa é a correta
 },
 {
-     enunciado: "O que um oncologista pediátrico faz?"
+     enunciado: "O que um oncologista pediátrico faz?",
      alternativas: [
           "Expecializado para cuidar da parte auditiva de uma criança",
           "Expecializado para cuidar de crianças com câncer"
@@ -31,7 +31,7 @@ const perguntas = [
      correta: 1 // A segunda alternativa é a correta
 },
 {
-     enunciado: "Normalmente, quantos litros de sangue uma pessoa tem?"
+     enunciado: "Normalmente, quantos litros de sangue uma pessoa tem?",
      alternativas: [
           "Tem de 2 a 4 litros",
           "Tem de 4 a 6 litros"
@@ -39,7 +39,7 @@ const perguntas = [
      correta: 1 // A segunda alternativa é a correta
 },
 {
-     enunciado: "De onde é a invenção do chuveiro elétrico?"
+     enunciado: "De onde é a invenção do chuveiro elétrico?",
      alternativas: [
           "Brasil",
           "França"
